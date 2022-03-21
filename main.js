34841070d418b744d835f156929a5420d0b30cad
Thiago Ribeiro <thisgo.014@gmail.com>
2022-03-21 13:36:19 -0300

    ✨ fix slider to bigger screens

--- main.js
+++ main.js
@@ -41,6 +41,12 @@ const swiper = new Swiper(".swiper-container", {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767:{
+      slidesPerView: 2,
+      setWrapperSize: true,
+    }
+  }
 });
 
 // Scroll Reveal mostrar elementos ao scrollar a pagina////
